0656e78b0ff1265e6e084127c06d4b612d843897
Paul Bagar <pbagar@xiphix.com>
2015-03-17 10:35:56 -0400

    initial commit

---
+++ .chef/knife.rb
@@ -0,0 +1,15 @@
+# See https://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "pbagar_xiphix"
+client_key               "#{current_dir}/pbagar_xiphix.pem"
+validation_client_name   "xiphix-validator"
+validation_key           "#{current_dir}/xiphix-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/xiphix"
+syntax_check_cache_path  "#{ENV['HOME']}/.chef/syntaxcache"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_copyright 		 "xiphix"
+cookbook_license		 "apachev2"
+cookbook_email			 "pbagar@xiphix.com"
---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'my_cookbook'
+maintainer       'xiphix'
+maintainer_email 'pbagar@xiphix.com'
+license          'Apache 2.0'
+description      'Installs/Configures my_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
